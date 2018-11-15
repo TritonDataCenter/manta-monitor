afc18e436f0489e84ec8eb6b8738148c7ab64e54
Sachin Gupta <sachingupta@joyentmac12331.local>
2018-11-15 15:48:26 -0800

    Adding request-$method percentile metrics to the custom prometheus collector.

--- src/main/java/com/joyent/manta/monitor/CustomPrometheusCollector.java
+++ src/main/java/com/joyent/manta/monitor/CustomPrometheusCollector.java
@@ -39,6 +39,15 @@ public class CustomPrometheusCollector extends Collector {
                     metricFamilySamples.add((new GaugeMetricFamily("requests_put_50thPercentile",
                             "Put Requests 50thPercentile Value",
                             jmxMetricsCollector.getMBeanAttributeValue("requests-put", "50thPercentile", Double.class))));
+                    metricFamilySamples.add((new GaugeMetricFamily("requests_put_75thPercentile",
+                            "Put Requests 75thPercentile Value",
+                            jmxMetricsCollector.getMBeanAttributeValue("requests-put", "75thPercentile", Double.class))));
+                    metricFamilySamples.add((new GaugeMetricFamily("requests_put_95thPercentile",
+                            "Put Requests 95thPercentile Value",
+                            jmxMetricsCollector.getMBeanAttributeValue("requests-put", "95thPercentile", Double.class))));
+                    metricFamilySamples.add((new GaugeMetricFamily("requests_put_99thPercentile",
+                            "Put Requests 99thPercentile Value",
+                            jmxMetricsCollector.getMBeanAttributeValue("requests-put", "99thPercentile", Double.class))));
                     metricFamilySamples.add((new CounterMetricFamily("requests_put_count",
                             "Put Requests Count",
                             jmxMetricsCollector.getMBeanAttributeValue("requests-put", "Count", Long.class).doubleValue())));
@@ -49,11 +58,68 @@ public class CustomPrometheusCollector extends Collector {
                     System.exit(1);
                 }
             }
+            if(jmxMetricsCollector.validateMbeanObject("requests-get")) {
+                try {
+                    metricFamilySamples.add((new GaugeMetricFamily("requests_put_mean",
+                            "Get Requests Mean Value",
+                            jmxMetricsCollector.getMBeanAttributeValue("requests-get", "Mean", Double.class))));
+                    metricFamilySamples.add((new GaugeMetricFamily("requests_get_50thPercentile",
+                            "Get Requests 50thPercentile Value",
+                            jmxMetricsCollector.getMBeanAttributeValue("requests-get", "50thPercentile", Double.class))));
+                    metricFamilySamples.add((new GaugeMetricFamily("requests_get_75thPercentile",
+                            "Get Requests 75thPercentile Value",
+                            jmxMetricsCollector.getMBeanAttributeValue("requests-get", "75thPercentile", Double.class))));
+                    metricFamilySamples.add((new GaugeMetricFamily("requests_get_95thPercentile",
+                            "Get Requests 95thPercentile Value",
+                            jmxMetricsCollector.getMBeanAttributeValue("requests-get", "95thPercentile", Double.class))));
+                    metricFamilySamples.add((new GaugeMetricFamily("requests_get_99thPercentile",
+                            "Get Requests 99thPercentile Value",
+                            jmxMetricsCollector.getMBeanAttributeValue("requests-get", "99thPercentile", Double.class))));
+                    metricFamilySamples.add((new CounterMetricFamily("requests_get_count",
+                            "Get Requests Count",
+                            jmxMetricsCollector.getMBeanAttributeValue("requests-get", "Count", Long.class).doubleValue())));
+                } catch (Exception e) {
+                    if(e instanceof ExceptionContext) {
+                        LOG.error("Failed to get the metric for reason: {}", e.getMessage());
+                    }
+                    System.exit(1);
+                }
+            }
+            if(jmxMetricsCollector.validateMbeanObject("requests-delete")) {
+                try {
+                    metricFamilySamples.add((new GaugeMetricFamily("requests_delete_mean",
+                            "Delete Requests Mean Value",
+                            jmxMetricsCollector.getMBeanAttributeValue("requests-delete", "Mean", Double.class))));
+                    metricFamilySamples.add((new GaugeMetricFamily("requests_delete_50thPercentile",
+                            "Delete Requests 50thPercentile Value",
+                            jmxMetricsCollector.getMBeanAttributeValue("requests-delete", "50thPercentile", Double.class))));
+                    metricFamilySamples.add((new GaugeMetricFamily("requests_delete_75thPercentile",
+                            "Delete Requests 75thPercentile Value",
+                            jmxMetricsCollector.getMBeanAttributeValue("requests-delete", "75thPercentile", Double.class))));
+                    metricFamilySamples.add((new GaugeMetricFamily("requests_delete_95thPercentile",
+                            "Delete Requests 95thPercentile Value",
+                            jmxMetricsCollector.getMBeanAttributeValue("requests-delete", "95thPercentile", Double.class))));
+                    metricFamilySamples.add((new GaugeMetricFamily("requests_delete_99thPercentile",
+                            "Delete Requests 99thPercentile Value",
+                            jmxMetricsCollector.getMBeanAttributeValue("requests-delete", "99thPercentile", Double.class))));
+                    metricFamilySamples.add((new CounterMetricFamily("requests_delete_count",
+                            "Delete Requests Count",
+                            jmxMetricsCollector.getMBeanAttributeValue("requests-delete", "Count", Long.class).doubleValue())));
+                } catch (Exception e) {
+                    if(e instanceof ExceptionContext) {
+                        LOG.error("Failed to get the metric for reason: {}", e.getMessage());
+                    }
+                    System.exit(1);
+                }
+            }
             if(jmxMetricsCollector.validateMbeanObject("retries")) {
                 try {
-                    metricFamilySamples.add(new CounterMetricFamily("Retry",
+                    metricFamilySamples.add(new CounterMetricFamily("retry",
                             "Number of Retries",
                             jmxMetricsCollector.getMBeanAttributeValue("retries", "Count", Long.class).doubleValue()));
+                    metricFamilySamples.add((new GaugeMetricFamily("retries_mean_rate",
+                            "Mean rate for number of retries",
+                            jmxMetricsCollector.getMBeanAttributeValue("retries", "MeanRate", Double.class))));
                 } catch (Exception e) {
                     if(e instanceof ExceptionContext) {
                         LOG.error("Failed to get the metric for reason: {}", e.getMessage());
