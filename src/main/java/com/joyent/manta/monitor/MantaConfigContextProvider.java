a020584bb3fcae5d2388aa2cac99f0a8852581d9
Elijah Zupancic <elijah@zupancic.name>
2019-01-11 16:44:29 -0800

    Improved configuration validation

--- src/main/java/com/joyent/manta/monitor/MantaConfigContextProvider.java
+++ src/main/java/com/joyent/manta/monitor/MantaConfigContextProvider.java
@@ -9,8 +9,9 @@ package com.joyent.manta.monitor;
 
 import com.joyent.manta.config.ConfigContext;
 import com.joyent.manta.config.MapConfigContext;
-import com.joyent.manta.config.StandardConfigContext;
+import com.joyent.manta.config.MetricReporterMode;
 import com.joyent.manta.config.SystemSettingsConfigContext;
+import com.joyent.manta.exception.ConfigurationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -24,14 +25,8 @@ import java.util.Properties;
 public class MantaConfigContextProvider implements Provider<ConfigContext> {
     private static final Logger LOG = LoggerFactory.getLogger(MantaConfigContextProvider.class);
     private static final int DEFAULT_TIMEOUT = 5_000; // 5 seconds in milliseconds
+    private static final int DEFAULT_PRUNE_DIR_DEPTH = 4;
 
-    private final StandardConfigContext appDefaults = new StandardConfigContext();
-
-    {
-        appDefaults.setRetries(0)
-                   .setTimeout(DEFAULT_TIMEOUT)
-                   .setTcpSocketTimeout(DEFAULT_TIMEOUT);
-    }
 
     public MantaConfigContextProvider() {
     }
@@ -44,14 +39,46 @@ public class MantaConfigContextProvider implements Provider<ConfigContext> {
         properties.putIfAbsent(MapConfigContext.MANTA_RETRIES_KEY, 0);
         properties.putIfAbsent(MapConfigContext.MANTA_TIMEOUT_KEY, DEFAULT_TIMEOUT);
         properties.putIfAbsent(MapConfigContext.MANTA_TCP_SOCKET_TIMEOUT_KEY, DEFAULT_TIMEOUT);
+        properties.putIfAbsent(MapConfigContext.MANTA_PRUNE_EMPTY_PARENT_DEPTH_KEY, DEFAULT_PRUNE_DIR_DEPTH);
 
         /* We explicitly pass the sysprops to the constructor, so that our
          * defaults will be applied. */
         final ConfigContext config = new SystemSettingsConfigContext(true, properties);
 
         LOG.debug("Manta configuration: {}", config);
+
+        validate(config);
+
         LOG.info("Request timeout is {} ms", config.getTimeout());
 
         return config;
+    }
+
+    /**
+     * Validates that the passed {@link ConfigContext} instance is valid for
+     * use with Manta Monitor.
+     *
+     * @param config instance to validate
+     */
+    private static void validate(final ConfigContext config) {
+        final MetricReporterMode configuredMode = config.getMetricReporterMode();
+
+        if (!MetricReporterMode.JMX.equals(configuredMode)) {
+            String msg = "Metric reporter mode must be set to JMX for "
+                    + "Manta Monitor to operate correctly. Actual setting: %s\n";
+            System.err.printf(msg, configuredMode);
+            System.exit(-1);
+        }
+
+        try {
+            ConfigContext.validate(config);
+        } catch (ConfigurationException e) {
+            final String msg = "Settings required for the correct "
+                    + "functioning of Java Manta SDK were not set. Please refer "
+                    + "to: https://github.com/joyent/java-manta/blob/master/USAGE.md#parameters";
+            System.err.println(msg);
+            System.err.println(e);
+            System.exit(1);
+        }
     }
 }
