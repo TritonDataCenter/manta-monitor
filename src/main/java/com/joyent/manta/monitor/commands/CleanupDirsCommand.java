e29b8f6acb9e8150ebef6c66a6d6047376525620
Elijah Zupancic <elijah@zupancic.name>
2019-01-31 09:55:28 -0800

    Fixes #25 (#26)

--- src/main/java/com/joyent/manta/monitor/commands/CleanupDirsCommand.java
+++ src/main/java/com/joyent/manta/monitor/commands/CleanupDirsCommand.java
@@ -8,9 +8,12 @@
 package com.joyent.manta.monitor.commands;
 
 import com.joyent.manta.client.MantaClient;
+import com.joyent.manta.exception.MantaClientHttpResponseException;
 import com.joyent.manta.monitor.MantaOperationContext;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang.StringUtils;
+
+import java.io.IOException;
 
 import static com.joyent.manta.client.MantaClient.SEPARATOR;
 
@@ -29,18 +32,37 @@ public class CleanupDirsCommand implements MantaOperationCommand {
         final String lowestDirWithoutBase = StringUtils.substringAfter(lowestDir, baseDir);
         final String[] dirs = StringUtils.split(lowestDirWithoutBase, SEPARATOR);
 
+        /* We want to delete only the files and directories related to the
+         * directory structure in the file sent in this chain and no others. */
         for (int i = dirs.length - 1; i >= 0; i--) {
             StringBuilder dir = new StringBuilder(baseDir);
             for (int j = 0; j <= i; j++) {
                 dir.append(SEPARATOR).append(dirs[j]);
             }
 
-            client.delete(dir.toString());
+            deleteObject(client, dir.toString());
         }
 
-        client.delete(baseDir);
+        deleteObject(client, baseDir);
 
         context.getStopWatch().stop();
         return PROCESSING_COMPLETE;
+    }
+
+    private static void deleteObject(final MantaClient client,
+                                     final String object) throws IOException {
+        try {
+            client.delete(object);
+        } catch (MantaClientHttpResponseException e) {
+            switch (e.getServerCode()) {
+                case RESOURCE_NOT_FOUND_ERROR:
+                    // do nothing because the object is already gone
+                case DIRECTORY_NOT_EMPTY_ERROR:
+                    // do nothing because the directory contains other objects
+                    break;
+                default:
+                    throw e;
+            }
+        }
     }
 }
